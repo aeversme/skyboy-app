f19e8310b705ce653757769e68dd529a5cb17bc9
Alex <aeversme@gmail.com>
2022-04-05 23:26:57 -0400

    change sed separators

--- .github/scripts/tf_files.sh
+++ .github/scripts/tf_files.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 sed "s/WORKSPACE_NAME/$WORKSPACE/g" ./terraform/backends.template > ./terraform/backends.tf
-sed "s/IMAGE_URI/$IMAGE/g" ./terraform/containers/task-definitions/skyboy.template > ./terraform/containers/task-definitions/skyboy.json
+sed "s|IMAGE_URI|$IMAGE|g" ./terraform/containers/task-definitions/skyboy.template > ./terraform/containers/task-definitions/skyboy.json
 cat ./terraform/loadbalancing/$LISTENERS >> ./terraform/loadbalancing/main.template
 mv ./terraform/loadbalancing/main.template ./terraform/loadbalancing/main.tf
 
-cat ./terraform/backends.tf
 cat ./terraform/containers/task-definitions/skyboy.json
-cat ./terraform/loadbalancing/main.tf
