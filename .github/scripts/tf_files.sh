9af37881d8c742bf71b063bbbef6fa27b9a59d01
Alex <aeversme@gmail.com>
2022-04-05 23:15:46 -0400

    edit tf_files.sh

--- .github/scripts/tf_files.sh
+++ .github/scripts/tf_files.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 sed "s/WORKSPACE_NAME/$WORKSPACE/g" ./terraform/backends.template > ./terraform/backends.tf
-cat ./terraform/backends.tf
 sed "s/IMAGE_URI/$IMAGE/g" ./terraform/containers/task-definitions/skyboy.template > ./terraform/containers/task-definitions/skyboy.json
-cat ./terraform/containers/task-definitions/skyboy.json
 cat ./terraform/loadbalancing/$LISTENERS >> ./terraform/loadbalancing/main.template
 mv ./terraform/loadbalancing/main.template ./terraform/loadbalancing/main.tf
+
+cat ./terraform/backends.tf
+cat ./terraform/containers/task-definitions/skyboy.json
 cat ./terraform/loadbalancing/main.tf
