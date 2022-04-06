49828d7c3439b40dbfd435653fd3c9de49529c45
Alex <aeversme@gmail.com>
2022-04-05 23:12:49 -0400

    change listeners file path

--- .github/scripts/tf_files.sh
+++ .github/scripts/tf_files.sh
@@ -4,6 +4,6 @@ sed "s/WORKSPACE_NAME/$WORKSPACE/g" ./terraform/backends.template > ./terraform/
 cat ./terraform/backends.tf
 sed "s/IMAGE_URI/$IMAGE/g" ./terraform/containers/task-definitions/skyboy.template > ./terraform/containers/task-definitions/skyboy.json
 cat ./terraform/containers/task-definitions/skyboy.json
-cat $LISTENERS >> ./terraform/loadbalancing/main.template
+cat ./terraform/loadbalancing/$LISTENERS >> ./terraform/loadbalancing/main.template
 mv ./terraform/loadbalancing/main.template ./terraform/loadbalancing/main.tf
 cat ./terraform/loadbalancing/main.tf
