2c67833f37fc91f5497dbf5ea912aa4d67d138b3
Alex <aeversme@gmail.com>
2022-04-06 22:00:27 -0400

    add initial terraform steps to workflow

--- .github/scripts/tf_files.sh
+++ .github/scripts/tf_files.sh
@@ -4,5 +4,3 @@ sed "s/WORKSPACE_NAME/$WORKSPACE/g" ./terraform/backends.template > ./terraform/
 sed "s|IMAGE_URI|$IMAGE|g" ./terraform/containers/task-definitions/skyboy.template > ./terraform/containers/task-definitions/skyboy.json
 cat ./terraform/loadbalancing/$LISTENERS >> ./terraform/loadbalancing/main.template
 mv ./terraform/loadbalancing/main.template ./terraform/loadbalancing/main.tf
-
-cat ./terraform/containers/task-definitions/skyboy.json
