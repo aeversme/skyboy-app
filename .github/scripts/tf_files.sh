f12861e66c42b3f7727d670da57002f2f1bbe7bd
Alex <aeversme@gmail.com>
2022-04-06 22:53:04 -0400

    edit paths in tf_files.sh

--- .github/scripts/tf_files.sh
+++ .github/scripts/tf_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed "s/WORKSPACE_NAME/$WORKSPACE/g" ./terraform/backends.template > ./terraform/backends.tf
-sed "s|IMAGE_URI|$IMAGE|g" ./terraform/containers/task-definitions/skyboy.template > ./terraform/containers/task-definitions/skyboy.json
-cat ./terraform/loadbalancing/$LISTENERS >> ./terraform/loadbalancing/main.template
-mv ./terraform/loadbalancing/main.template ./terraform/loadbalancing/main.tf
+sed "s/WORKSPACE_NAME/$WORKSPACE/g" ./backends.template > ./backends.tf
+sed "s|IMAGE_URI|$IMAGE|g" ./containers/task-definitions/skyboy.template > ./containers/task-definitions/skyboy.json
+cat ./loadbalancing/$LISTENERS >> ./loadbalancing/main.template
+mv ./loadbalancing/main.template ./loadbalancing/main.tf
