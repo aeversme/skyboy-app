c337b9131e6d7f67f6436e3ab59f351d9b9c9915
Alex <aeversme@gmail.com>
2022-04-07 09:49:42 -0400

    add path to workflow and create main_apply_tf

--- .github/scripts/tf_creds.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "credentials \"app.terraform.io\" {" > tf.tfrc
-echo "  token = \"$TOKEN\"" >> tf.tfrc
-echo "}" >> tf.tfrc
-mv tf.tfrc $HOME/tf.tfrc
-export TF_CLI_CONFIG_FILE="$HOME/tf.tfrc"
