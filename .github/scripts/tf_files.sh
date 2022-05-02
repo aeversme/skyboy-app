85856712459366ebcd177081497aadc26fb68d4c
Alex <aeversme@gmail.com>
2022-05-01 20:33:34 -0400

    revise task definition to handle SMPS secrets

--- .github/scripts/tf_files.sh
+++ .github/scripts/tf_files.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 sed "s/WORKSPACE_NAME/$WORKSPACE/g" ./backends.template > ./backends.tf
-sed "s|IMAGE_URI|$IMAGE|g" ./containers/task-definitions/skyboy.template > ./containers/task-definitions/skyboy.json
+sed "s|IMAGE_URI|$IMAGE|g" ./containers/task-definitions/skyboy.template |
+  sed "s|USER_ID_ARN|$USER_ID_ARN|g" |
+  sed "s|USER_KEY_ARN|$USER_KEY_ARN|g" > ./containers/task-definitions/skyboy.json
 cat ./loadbalancing/$LISTENERS >> ./loadbalancing/main.template
 mv ./loadbalancing/main.template ./loadbalancing/main.tf
