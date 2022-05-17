bcb06c3b904004051d0c744a89795ffbfb353f0d
Alex <aeversme@gmail.com>
2022-05-16 20:13:54 -0400

    add Datadog IAM and task definition integration

--- terraform/iam/main.tf
+++ terraform/iam/main.tf
@@ -11,6 +11,23 @@ data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
   }
 }
 
+data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::464622532012:root"]
+    }
+    condition {
+      test = "StringEquals"
+      variable = "sts:ExternalId"
+
+      values = [var.datadog_aws_integration_external_id]
+    }
+  }
+}
+
 data "aws_iam_policy_document" "ecs_task_policy" {
   statement {
     actions = [
@@ -28,10 +45,28 @@ data "aws_iam_policy_document" "ecs_task_policy" {
   }
 }
 
+data "aws_iam_policy_document" "datadog_aws_integration" {
+  statement {
+    actions = [
+      "ecs:ListClusters",
+      "ecs:ListContainerInstances",
+      "ecs:DescribeContainerInstances"
+    ]
+
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "ecs_task_role" {
   name               = "ecsTaskRole"
   path               = "/system/"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
+}
+
+resource "aws_iam_role" "datadog_aws_integration" {
+  name = "DatadogAWSIntegrationRole"
+  description = "Role for Datadog AWS Integration"
+  assume_role_policy = data.aws_iam_policy_document.datadog_aws_integration_assume_role.json
 }
 
 resource "aws_iam_policy" "ecs_task_policy" {
@@ -40,7 +75,17 @@ resource "aws_iam_policy" "ecs_task_policy" {
   policy      = data.aws_iam_policy_document.ecs_task_policy.json
 }
 
+resource "aws_iam_policy" "datadog_aws_integration" {
+  name = "DatadogAWSIntegrationPolicy"
+  policy = data.aws_iam_policy_document.datadog_aws_integration.json
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_policy_attachment" {
   policy_arn = aws_iam_policy.ecs_task_policy.arn
   role       = aws_iam_role.ecs_task_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "datadog_aws_integration" {
+  role = aws_iam_role.datadog_aws_integration.name
+  policy_arn = aws_iam_policy.datadog_aws_integration.arn
 }
